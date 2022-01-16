21e2bbfc56029787b67358c340b80ed8c4c68fd3
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-16 11:36:38 +0100

    2nd Commit

---
+++ src/test/java/dev/citest/CiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.citest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
